74d06f0ccf34c321799a0bd3ae93ba872ca11c12
Jannik Busse <jannik.busse@rwth-aachen.de>
2021-10-29 22:35:48 +0200

    added basic project structure

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	std::cout << "this is the main program file" << std::endl;
+	return 0;
+}
---
+++ src/mprocessor/music.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	std::cout << "this is the music processor demo program" << std::endl;
+	return 0;
+}

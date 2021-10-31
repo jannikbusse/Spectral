520338faa60b96573e27741746076d9467d9ed99
Jannik Busse <jannik.busse@rwth-aachen.de>
2021-10-31 21:50:50 +0100

    implemented a simple 7 band equalizer

--- include/math/math.hpp
+++ include/math/math.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <math.h>
 #include <string.h>
 #include <stdio.h>
 #include <iostream>
@@ -7,7 +8,7 @@
 namespace math
 {
 
-    float getPeakAmplitude(float* signal, int bufferSize)
+    float getPeakAmplitude(const float* signal, int bufferSize)
     {
         float peak = 0;
         for(int i = 0; i < bufferSize; i ++)
@@ -18,6 +19,55 @@ namespace math
             }
         }
         return peak;
+    }
+
+    void normBuffer(float *buffer, int bufferSize)
+    {
+        double peak = 0;
+        for(int i = 0; i < bufferSize; i ++)
+        {
+            if(abs(buffer[i]) > peak)
+            {
+                peak = abs(buffer[i]);
+            }
+        }
+        for(int i = 0; i < bufferSize; i ++)
+        {
+            buffer[i] /= peak;
+        }
+    }
+
+    void getScaledFrequencyBands(fftw_complex rawSignal[], float (&sequencyBands)[6][2])
+    {
+        //this will be hardcoded for 32 size input and 6 size output.. this should be implemented better
+        //the deltafrequency is 100 with these values (which is nice)
+            for(int a = 0; a < 2; a++)
+                {
+                sequencyBands[0][a] = 0;
+                for(int i = 0; i <2; i++){
+                    sequencyBands[0][a] += rawSignal[i][a];
+                }
+                sequencyBands[1][a] = 0;
+                for(int i = 2; i <4; i++){
+                    sequencyBands[1][a] += rawSignal[i][a];
+                }
+                sequencyBands[2][a] = 0;
+                for(int i = 4; i <8; i++){
+                    sequencyBands[2][a] += rawSignal[i][a];
+                }
+                sequencyBands[3][a] = 0;
+                for(int i = 8; i <16; i++){
+                    sequencyBands[3][a] += rawSignal[i][a];
+                }
+                sequencyBands[4][a] = 0;
+                for(int i = 16; i <32; i++){
+                    sequencyBands[4][a] += rawSignal[i][a];
+                }
+                sequencyBands[5][a] = 0;
+                for(int i = 32; i <50; i++){
+                    sequencyBands[5][a] += rawSignal[i][a];
+                }
+            }            
     }
     
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,21 @@
 #include <utils/microphone.hpp>
 #include <math/math.hpp>
 #include <string.h>
-
-#define NUM_POINTS 512
-#define SAMPLE_DURATION 3.14
-
-
-float sampleFreq = NUM_POINTS / SAMPLE_DURATION;
-
-
-/* Never mind this bit */
-
 #include <stdio.h>
 #include <math.h>
+
+#define NUM_POINTS 128
+#define SAMPLE_DURATION 0.01
+
+using namespace std;
 
 #define REAL 0
 #define IMAG 1
 
-using namespace std;
+float sampleFreq = NUM_POINTS / SAMPLE_DURATION;
+float *magnitudes = (float*) malloc(sizeof(float) * NUM_POINTS/2);
+
+float scaledFreqBand[6][2];
 
 void acquire_from_somewhere(fftw_complex* signal) {
     /* Generate two sine waves of different frequencies and
@@ -28,56 +26,96 @@ void acquire_from_somewhere(fftw_complex* signal) {
      */
 
     int i;
-    for (i = 0; i < NUM_POINTS; ++i) {
-        double theta = (double)i / (double)NUM_POINTS * SAMPLE_DURATION;
+    for (i = 0; i < NUM_POINTS; i++) {
+        float theta = ((float)i / (float)NUM_POINTS) * SAMPLE_DURATION;
 
-        signal[i][REAL] = 1.0 * cos(10.0 * theta) +
-                          0.5 * cos(25.0 * theta);
+        signal[i][REAL] = 1.0 * cos(510.0 * 2*M_PI * theta) +  2.0 * cos(300.0 * 2*M_PI * theta);
 
-        cout << signal[i][REAL] << endl;
 
         signal[i][IMAG] = 0;
     }
 }
 
 void do_something_with(fftw_complex* result) {
-    int i;
     float frequencydelta  = sampleFreq / NUM_POINTS;
 
-    for (i = 0; i < NUM_POINTS / 8; ++i) {
-        double mag = sqrt(result[i][REAL] * result[i][REAL] +
-                          result[i][IMAG] * result[i][IMAG]);
+    math::getScaledFrequencyBands(result, scaledFreqBand);
+    scaledFreqBand[6][REAL] = 5;
 
-        printf("%g: %g\n", (frequencydelta * i), mag);
+
+    for (int i = 0; i < 6; i++) {
+        float mag = sqrt(scaledFreqBand[i][REAL] * scaledFreqBand[i][REAL] +
+                          scaledFreqBand[i][IMAG] * scaledFreqBand[i][IMAG]);
+
+        magnitudes[i] = mag;
     }
+    //math::normBuffer(magnitudes, NUM_POINTS/2);
+    std::system("clear");
+
+    for (int i = 0; i < 6; i++) {
+        string out = "";
+       
+        out = to_string((int)(frequencydelta *     pow(2,i+1)))  ;
+        //out = out + " " + to_string(magnitudes[i]);
+        for(int a = 0; a < log10(magnitudes[i]) * 5; a ++)
+        {
+            out =  out + "#";
+        }
+        cout << out << endl;
+
+    }
+    cout << "frequency max: " << (sampleFreq/2) << endl;
+
 }
 
 
 /* Resume reading here */
 
-using namespace std;
 
-int main() {
+int startMicRoutine()
+{
     float* buffer = (float*) malloc(sizeof(float) * 512);
-    initMicrophone();
+    initMicrophone(sampleFreq, NUM_POINTS);
+
+    fftw_complex signal[NUM_POINTS];
+    fftw_complex result[NUM_POINTS];
+
+    for(int i = 0; i < NUM_POINTS; i ++)
+    {
+        signal[i][IMAG] = 0;
+    }
+
     while(1){
-        Pa_Sleep(2);
+        Pa_Sleep(1);
         if (!readCurrentBuffer(buffer))
         {
-            float peak = math::getPeakAmplitude(buffer, 512) * 20;
-            string out = "";
-            for(int i = 0; i < peak; i ++)
-            {
-                out = out + "#";
-            }
-            std::system("clear");
-            cout << out << endl;
+            
+            fftw_plan plan = fftw_plan_dft_1d(NUM_POINTS,
+                                            signal,
+                                            result,
+                                            FFTW_FORWARD,
+                                            FFTW_ESTIMATE);
 
+            for(int i = 0; i < NUM_POINTS; i ++)
+            {
+                signal[i][REAL] = buffer[i];
+            }
+            fftw_execute(plan);
+            do_something_with(result);
+            fftw_destroy_plan(plan);
 
         }
+        //cout << "buffer miss" << endl;
+
     }
+    
+
     releaseMicrophone();
-    exit(0);
+}
+
+
+int main() {
+    startMicRoutine();   
 
     fftw_complex signal[NUM_POINTS];
     fftw_complex result[NUM_POINTS];
@@ -94,7 +132,6 @@ int main() {
 
     fftw_destroy_plan(plan);
 
-    cout << "frequency max: " << (sampleFreq/2) << endl;
 
     return 0;
 }

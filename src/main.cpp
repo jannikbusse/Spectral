2dae6004e5e41f28d726e56f9069eea7bdae27c8
Jannik Busse <jannik.busse@rwth-aachen.de>
2021-11-04 20:08:56 +0100

    gibt es eine einigermassen funktionierende tempoestimation? ich glaube ja

--- src/main.cpp
+++ src/main.cpp
@@ -17,48 +17,42 @@
 #include "gnuplot-iostream.h"
 
 #define OUTPUT_ENABLED 1
-
 #define NUM_POINTS 128
-#define SAMPLE_DURATION 0.01
-
-#define NUMBER_OF_FRAMES 400
-
+#define SAMPLE_DURATION 0.05
+#define NUMBER_OF_FRAMES 500
 #define MINIMAL_SIGNAL_THRESHOLD 0.05 //this is the threshold for the noise reduction
+#define NUMBER_OF_COMBFILTERS 100
 
 using namespace std;
 
 #define REAL 0
 #define IMAG 1
 
+float GAIN_PER_FREQUENCY[] = {4, 3, 2, 1, 1, 1};
 float sampleFreq = NUM_POINTS / SAMPLE_DURATION;
 float scaledFreqBand[6];
 float unprocessedMagnitudes[NUM_POINTS/2];
 float magnitues[6][NUMBER_OF_FRAMES];
 float magnitudeDifferentials[6][NUMBER_OF_FRAMES];
 
-float allignedBuffer[NUMBER_OF_FRAMES];
-float ringBufferDifferentialOverTime[NUMBER_OF_FRAMES];
+float alignedSumDifferentialBuffer[NUMBER_OF_FRAMES]; //this is the plotting buffer fo the ringbufferdifferentialovertime
+float ringBufferDifferentialOverTime[NUMBER_OF_FRAMES]; // this stores the summed up differential over time
 
 float rollingAverage[6];
-float alignedRollingDifferentialSumAverage[NUMBER_OF_FRAMES];
-float rollingDifferentialSumAverage[NUMBER_OF_FRAMES];
+float alignedRollingDifferentialSumAverage[NUMBER_OF_FRAMES]; // this buffer is only for plotting. Old Averages have to be kept over time. This is the buffer that gets aligned
+float rollingDifferentialSumAverage[NUMBER_OF_FRAMES]; //this buffer is only for plotting. Old Averages have to be kept over time
 
+float combFilterbank[6][NUMBER_OF_COMBFILTERS]; //this buffers stores the summed up energy of each comb filter
 
-float loopFrequency = 4000;
-
-fftwf_complex magDifferentialResults[NUMBER_OF_FRAMES];
-
-float updatesPerSecond = 10;
-
-
-
-
+float loopFrequencyDelta = 1; //this value gets updated each loop. It is the frequency Delta
+float updatesPerSecond = 24; //number of updates of gnuplot per second
 
 float frequencydelta  = sampleFreq / NUM_POINTS;
 
+fftwf_complex magDifferentialResults[NUMBER_OF_FRAMES];
+
 //the buffer index for the ringbuffers
 int magBufferIdx = 0;
-
 //just and counter that counted how often there were not enough audiosamples when accessed. It is no longer required.
 int bufferMisses = 0;
 
@@ -73,9 +67,7 @@ void acquire_from_somewhere(float* signal) {
     int i;
     for (i = 0; i < NUM_POINTS; i++) {
         float theta = ((float)i / (float)NUM_POINTS) * SAMPLE_DURATION;
-
         signal[i] = 1.0 * cos(510.0 * 2*M_PI * theta) +  2.0 * cos(300.0 * 2*M_PI * theta );
-
     }
 }
 
@@ -83,10 +75,9 @@ void acquire_from_somewhere(float* signal) {
 void plotDifferentialBuffer()
 {
    
-       
     std::vector<std::pair<double, double> > xy_pts_A;
     for(int x=0; x< NUMBER_OF_FRAMES; x++) {
-        double y = allignedBuffer[x];
+        double y = alignedSumDifferentialBuffer[x];
         xy_pts_A.push_back(std::make_pair(x, y));
     }
     std::vector<std::pair<double, double> > xy_pts_B;
@@ -115,22 +106,64 @@ void suppressNoiseAmp(float* signal)
     }
 }
 
-int detectSignalOnset(int frequencyBin)
+
+void alignMagnitudeRingBuffers()
+{
+    for(int i = 0; i < NUMBER_OF_FRAMES; i ++)
+    {
+        alignedSumDifferentialBuffer[i] = ringBufferDifferentialOverTime[(i + magBufferIdx) % NUMBER_OF_FRAMES];
+        alignedRollingDifferentialSumAverage[i] = rollingDifferentialSumAverage[(i + magBufferIdx) % NUMBER_OF_FRAMES];
+         
+    }
+}
+
+void applyCombFilterToFrequencyBand(int bandId)
 {
    
 
+    //apply comb filters
+    //the comb filter with delay T: y_t = alpha * y_(t-T) + ( 1 - alpha ) * x_t 
+    //for alpha = 0.5^(t/T)
+    //note that the frequencyDelta is given by SAMPLE TIME
+    //loopfrequencydelta is given in sec
+    
+    float delay_T = 1; //in seconds
+    float alpha = 0;
+    float numberOfTimeSteps = 0;
+    for(int i = 0; i < NUMBER_OF_COMBFILTERS; i++)
+    {
+        delay_T = 1 * i;
+        numberOfTimeSteps = delay_T/SAMPLE_DURATION;
+        for(int t = 0; t < NUMBER_OF_FRAMES; t++)
+        {
+            alpha = pow(0.5, (t/delay_T));
+            
+            combFilterbank[bandId][magBufferIdx] = magnitudeDifferentials[bandId][magBufferIdx];
+            //add the comb filter signal
+            combFilterbank[bandId][magBufferIdx] += (1-alpha)*combFilterbank[bandId][magBufferIdx];
+            //att the first iteration of the filter
+            
+        }
+        cout << numberOfTimeSteps << endl;
+
+
+    }
+    cout << NUMBER_OF_FRAMES * SAMPLE_DURATION <<endl;
+
+
 }
+
 
 void findDominantFrequency()
 {
     for(int i = 0; i < NUMBER_OF_FRAMES; i ++)
     {
-        allignedBuffer[i] = ringBufferDifferentialOverTime[(i + magBufferIdx) % NUMBER_OF_FRAMES];
+        alignedSumDifferentialBuffer[i] = ringBufferDifferentialOverTime[(i + magBufferIdx) % NUMBER_OF_FRAMES];
         alignedRollingDifferentialSumAverage[i] = rollingDifferentialSumAverage[(i + magBufferIdx) % NUMBER_OF_FRAMES];
-         
+
     }
     fftwf_plan plan = fftwf_plan_dft_r2c_1d( NUMBER_OF_FRAMES,
-                                            allignedBuffer,
+                                            alignedSumDifferentialBuffer,
                                             magDifferentialResults,
                                             0);
 
@@ -139,13 +172,12 @@ void findDominantFrequency()
 
 
     //now find the dominant frequency!!
-    float diffSampleFreq = 1000000 / loopFrequency;
-    float diffFreqDelta = loopFrequency / (NUMBER_OF_FRAMES);
+    float diffFreqDelta = (1/SAMPLE_DURATION) / (NUMBER_OF_FRAMES);
 
     int dominantBin = 0;
     float dominantAmp = 0;
-    //std::system("clear");
-    for(int i = 0; i <  NUMBER_OF_FRAMES/130; i ++)
+    std::system("clear");
+    for(int i = 4; i <  NUMBER_OF_FRAMES/10; i ++)
     {
         float curAmp = sqrt(magDifferentialResults[i][REAL] * magDifferentialResults[i][REAL] + magDifferentialResults[i][IMAG] * magDifferentialResults[i][IMAG]);
         if(curAmp > dominantAmp){
@@ -153,20 +185,21 @@ void findDominantFrequency()
             dominantBin = i;
         }
 
-        string out = to_string(i * diffFreqDelta  * 60);
+        string out = to_string((float)i * diffFreqDelta  * 60);
         for(int a = 0; a < curAmp ; a ++)
         {
             out = out + "#";
         }
-        //cout << out << endl;
+        cout << out << endl;
     }
-    //cout << "dominant frequency " << ((float)dominantBin * diffFreqDelta * 60.0) << endl;
+    cout << "dominant frequency " << ((float)dominantBin * diffFreqDelta * 60.0) << endl;
+    cout << "time window length:" << NUMBER_OF_FRAMES * SAMPLE_DURATION << endl;
 
     fftwf_destroy_plan(plan);
 
-    
-
 }
+
+
 
 void processResults(fftwf_complex* result)
 {
@@ -174,7 +207,7 @@ void processResults(fftwf_complex* result)
     //first compute the magnitudes
     for(int i = 0; i < NUM_POINTS/2; i++)
     {
-        unprocessedMagnitudes[i] = (sqrt(result[i][REAL] * result[i][REAL] + result[i][IMAG] * result[i][IMAG]));
+        unprocessedMagnitudes[i] = ((sqrt(result[i][REAL] * result[i][REAL] + result[i][IMAG] * result[i][IMAG])));
         
     }
     //Now apply noise reduction to the unprocessed magnitudes
@@ -186,25 +219,27 @@ void processResults(fftwf_complex* result)
     for(int i = 0; i < 6; i++)
     {
         
-        magnitues[i][magBufferIdx] = ( (scaledFreqBand[i] * 0.2) + (0.8 * magnitues[i][previousMagBuffer]));
+        magnitues[i][magBufferIdx] = ( ( GAIN_PER_FREQUENCY[i] * scaledFreqBand[i] * 0.1) + (0.9 * magnitues[i][previousMagBuffer]));
     }
 
     //now calculate the discrete derivative of the magnitudes(and apply a small rolling average to the signal)
     for(int i = 0; i < 6; i++)
     {
-        float currentDifferential =  max(0.0f, magnitues[i][magBufferIdx] - magnitues[i][previousMagBuffer] - rollingAverage[i] - 1);
-        //float currentDifferential =  magnitues[i][magBufferIdx] - magnitues[i][previousMagBuffer] - rollingAverage[i];
-        rollingAverage[i] = rollingAverage[i] * 0.999 + currentDifferential * 0.001;
+        //float currentDifferential =  max(0.0f, magnitues[i][magBufferIdx] - magnitues[i][previousMagBuffer] - rollingAverage[i] - 1);
+        float currentDifferential =  magnitues[i][magBufferIdx] - magnitues[i][previousMagBuffer] - rollingAverage[i];
+        rollingAverage[i] = rollingAverage[i] * 0.997 + currentDifferential * 0.003;
 
         // magnitudeDifferentials[i][magBufferIdx] = magnitudeDifferentials[i][previousMagBuffer] * 0.005 + currentDifferential * 0.995 ;
-        magnitudeDifferentials[i][magBufferIdx] = magnitudeDifferentials[i][previousMagBuffer] * 0.8 + currentDifferential * 0.2 ;
+        magnitudeDifferentials[i][magBufferIdx] = magnitudeDifferentials[i][previousMagBuffer] * 0.87 + currentDifferential * 0.13 ;
     }
 
     //just for fun add up all current differentials
     float differentialSum = 0;
+    float averageSum = 0;
+
     for(int a = 0; a < 6; a ++)
     {
-        
+        averageSum += rollingAverage[i];
         differentialSum+= magnitudeDifferentials[a][magBufferIdx];
         
     }
@@ -215,11 +250,13 @@ void processResults(fftwf_complex* result)
     }
     cout << out << endl;
 
-    rollingDifferentialSumAverage[magBufferIdx] = differentialSum * 0.01 + rollingDifferentialSumAverage[previousMagBuffer] *  0.99;
+    rollingDifferentialSumAverage[magBufferIdx] = averageSum;
     ringBufferDifferentialOverTime[magBufferIdx] = differentialSum;
     
-
+    alignMagnitudeRingBuffers();
     findDominantFrequency();
+
+    //applyCombFilterToFrequencyBand(0);
 
     magBufferIdx = (magBufferIdx +1 ) % NUMBER_OF_FRAMES;
 
@@ -256,21 +293,13 @@ int startMicRoutine()
             bufferMisses = 0;
 
             auto stop = chrono::high_resolution_clock::now();
-            
-
             auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-            
-
-            if(duration < std::chrono::microseconds(16)){
-                //std::this_thread::sleep_for(std::chrono::milliseconds(16) - (duration));
-            }
-
-            loopFrequency = loopFrequency * 0.99 +  (duration.count())* 0.01;
+            loopFrequencyDelta = loopFrequencyDelta * 0.995 +  (duration.count())* 0.005;
   
             // To get the value of duration use the count()
             // member function on the duration object
             //if(OUTPUT_ENABLED)
-                // cout << "millisec per cycle"  << loopFrequency << endl;
+                // cout << "millisec per cycle"  << loopFrequencyDelta << endl;
                 // cout << "millisec per cycle"  << duration.count() << endl;
 
 
@@ -280,8 +309,11 @@ int startMicRoutine()
             auto lastUpdate = std::chrono::duration_cast<chrono::milliseconds>(stopUpdate - plotUpdateTimer);
             
             bufferMisses ++;
-        if(lastUpdate.count() > 1000/updatesPerSecond)
-            plotDifferentialBuffer();
+            if(lastUpdate.count() > 1000/updatesPerSecond)
+            {
+                plotUpdateTimer = chrono::high_resolution_clock::now();
+                plotDifferentialBuffer();
+            }
         }
 
     }
